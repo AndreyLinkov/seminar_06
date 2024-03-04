683cd2c48680ea5471f9c1de5d4a1b7bd114e600
Andrey Linkov <linkov.developer@inbox.ru>
2024-03-04 04:59:09 +0300

    add task_09

---
+++ task_09/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+using System;
+
+namespace task_09
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Clear();
+            string str = "My name is Andrey Suraev!";
+            Console.WriteLine(str);
+            string strLow = str.ToLower();
+            Console.WriteLine(strLow);
+        }
+    }
+}
